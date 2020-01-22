b25aa4636eb5e95636f2761b4fc86a20f12b02b0
masumi-abe49 <andfolk1129@gmail.com>
2020-01-22 10:39:23 +0900

    add_DOM（未完了）

---
+++ js/dom/question.js
@@ -0,0 +1,105 @@
+'use strict';
+// 例題 コンソールに数値「1」を出力してください。
+// console.log(1);
+
+// 問題1：要素の取得
+// id属性txt1の要素を取得し、変数txt1に代入してください。
+// 代入後、変数txt1をコンソールに表示してださい。
+let txt1 = document.getElementById('txt1');
+
+console.log(txt1);
+
+// 問題2：テキストの取得
+// id属性txt2の要素を取得し、変数txt2に代入してください。
+// txt2要素配下のテキストを取得し、コンソールに表示してください。
+// ただしHTMLタグを含むもの、含まないもの両方の方法で表示すること。
+
+let txt2 = document.getElementById('txt2');
+
+console.log(txt2.innerHTML);
+console.log(txt2.innerText);
+
+// 問題3：テキストの書き換え
+// id属性txt2配下のテキストを「書き換えました」という文字列に書き換えてください。
+
+txt2.innerText = "書き換えました";
+
+// console.log(txt2.innerText);
+
+
+// 問題4：イベント(クリック)
+// 取得ボタンをクリックしたとき、id属性txt3のテキストを取得し、id属性txt4に表示してください。
+
+document.getElementById("btn1").onclick = function() {
+  document.getElementById("txt4").innerHTML = document.getElementById("txt3").innerText;
+};
+
+
+// 問題5：イベント(サブミット) / フォーム部品の取得(テキスト)
+// 送信ボタンをクリックしたとき、フォーム部品(id属性input1のvalue)の値を取得し、id属性txt5に出力してください。
+
+// html内にbuttunタグで送信ボタンを作るi方がいいかも？
+
+let form = document.getElementById("form1");
+
+form.addEventListener("submit", function() {
+  document.getElementById("txt5").innerText = document.getElementById("input1").value;
+  document.form1.submit();
+  // return false;
+});
+
+// 問題6：フォーム部品の取得(セレクトボックス)
+// 送信ボタンをクリックしたとき、セレクトボックス(name属性item1のvalue)の選択中の値を取得し、id属性result1に出力してください。
+
+
+
+
+// 問題7：フォーム部品の取得(チェックボックス)
+// 送信ボタンをクリックしたとき、チェックボックス(name属性item1のvalue)の選択中の値を取得し、id属性result2に出力してください。
+
+
+// 問題8：フォーム部品の取得(ラジオ)
+// 送信ボタンをクリックしたとき、ラジオボタン(name属性item1のvalue)の選択中の値を取得し、id属性result3に出力してください。
+
+
+// 問題9：バリデーションチェック
+// 会社名が未入力のまま送信ボタンをクリックしたとき、「会社名が未入力です。」というアラートを表示してください。また未入力の場合は、処理を中断してください。
+
+
+// 問題10：CSSの操作
+// id属性txt6のCSSプロパティを以下に変更してください。
+// また変更するタイミングは画面表示時とする。
+// 文字色→赤色、フォントサイズ→32px
+
+
+// 問題11：ノード追加
+// 追加ボタンをクリックしたとき、id属性parentの子要素にpタグを追加し、pタグに「ノード追加テキスト」というテキストを追加してください。
+
+
+// 問題12：ノード削除
+// 削除ボタンをクリックしたとき、id属性parentの子要素を削除してください。
+
+
+
+// 問題13：属性取得・変更
+// 変更ボタンをクリックしたとき、id属性link1のhref属性を取得し、コンソールに表示してください。またhref属性を「https://www.yahoo.co.jp」に書き換えてください。
+
+
+
+// 問題14：クエリーセレクター / カスタム属性 / this
+// querySelectorAllを使い、id属性list1の子要素のliを全て取得してください。
+// liをクリックしたとき、カスタム属性numの値を取得し、コンソールに表示してください。
+// その場合、クリックした要素のカスタム属性のみ取得すること。
+// 例 list２のliをクリックしたときはコンソールに「2」を表示する
+
+
+// 問題15-1：クラスの操作(追加)
+// 追加ボタンをクリックしたとき、id属性txt7にクラスredを追加してください。
+
+
+// 問題15-2：クラスの操作(削除)
+// 削除ボタンをクリックしたとき、id属性txt7にクラスredを削除してください。
+
+
+// 問題15-3：クラスの操作(切り替え)
+// 切り替えボタンをクリックしたとき、id属性txt7にクラスredがあれば削除し、なければ追加してください。
