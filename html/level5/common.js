0d5cb84518bb95e59328ae36464da6e3984ea944
masumi-abe49 <andfolk1129@gmail.com>
2020-03-25 15:27:35 +0900

    fix_5_新規コーディング課題レベル5(アニメーション)

--- html/level5/common.js
+++ html/level5/common.js
@@ -1,48 +1,50 @@
 $(function() {
-  // ＊＊＊できてない＊＊＊ animation1　top-wrapperのアニメーション
+  // animation1　top-wrapperのアニメーション 順番に細かく拡大縮小
   // opacityを0にする
-  $('.top-wrapper h1, .top-wrapper p').css("opacity","0");
+  $('.top-wrapper h1, .border-a, .top-wrapper p').css("opacity","0");
 
   $(window).on('load',function(){
-    $('.top-wrapper h1, .top-wrapper p').each(function() {
-      $('.top-wrapper h1').addClass('zoomup');
-      setTimeout(function(){
-        $('.top-wrapper p').addClass('zoomup');
-      } ,250);
-      $(this).css("opacity","1");
-    });
+    $('.top-wrapper h1').addClass('top-animation');
+    // 順番に細かく拡大縮小
+    setTimeout(function(){
+      $('.top-wrapper h1').next().addClass('top-animation');
+      },250);
+    setTimeout(function(){
+      $('.top-wrapper h1').next().next().addClass('top-animation');
+      },500);  
   });
 
 
-  // $('.introduction-wrapper h2').css({"transform": "rotate(0deg)", "transition": "transform 2s"});
+  // animation2 INTRODUCTION 　introduction-wrapperのアニメーション　下線部のみ左から回転してくる
 
-  // ＊＊＊できてない　擬似要素の動きが違う＊＊＊ animation2 INTRODUCTION 　introduction-wrapperのアニメーション
+  $('.introduction-wrapper h2, .border-rotate, .introduction-wrapper p').css("opacity","0");
 
-  $('.introduction-wrapper h2, .introduction-wrapper p').css({"opacity": "0",
-  "transform": "scale(0)",
-  "transition": "transform 2s"});
-
-  $(window).on('scroll', function (e){
-    $('.introduction-wrapper h2, .introduction-wrapper p').each(function(){
+  $(window).on('scroll', function (){
+    $('.introduction-wrapper h2,  .border-rotate, .introduction-wrapper p').each(function(){
       let position = $(this).offset().top; //.offset().topで、ページの最上部から「.top-wrapper h1, .top-wrapper p」が付いた要素までの距離を取得して、変数positionに代入
       let scroll = $(window).scrollTop(); // .scrollTop()で、スクロールした量を取得して、変数scrollに代入
       let windowHeight = $(window).height(); // .height()で、ウィンドウの高さを取得して、変数windowHeightに代入します。
-      if (scroll > position - windowHeight + windowHeight/5){
-        $('.introduction-wrapper h2').addClass('active');
+      if (scroll > position - windowHeight + windowHeight/2){
+        $('.introduction-wrapper h2').addClass('zoom');
+        setTimeout(function(){
+          $('.introduction-wrapper h2').next().next().addClass('zoom');
+          },250);
+        $('.border-rotate').addClass('slide-rotate');
         $(this).css({
           "opacity": "1",
-          "transform": "scale(1)",
-          });
-      } else {
-        $(this).css({
-          "opacity": "0",
-          "transform": "scale(0)",
+          "transform": "scale(1)"
           });
       }
     });
   });
 
-  //***できていない*** */ animation3 （animation7と同じ感じ）＜CSS keyframes使ってみる＞ Pure Creativity 縦回転
+  // ↓以下部分を↑のif文に書いてしまうと変になるので、メモとしてコメントアウト
+  //  else {
+  //       $('.introduction-wrapper h2, .introduction-wrapper p').removeClass('zoom');
+  //       $('.border-rotate').removeClass('slide-rotate');
+  //     }
+
+  //***200323*** */ animation3 （animation7と同じ感じ）＜CSS keyframes使ってみる＞ Pure Creativity 縦回転
    $('.image-wrapper01 p, .image-wrapper04 p').css({
      "opacity": "0"
      });
@@ -67,7 +69,7 @@ $(function() {
     });
   });
 
-  // ＊＊＊微妙？？＊＊＊animation4  OUR CLIENTS ズームイン＋logo順番にフェードイン
+  // ＊＊＊200323＊＊＊animation4  OUR CLIENTS ズームイン＋logo順番にフェードイン
   $('.logo-wrapper h2, .logo-wrapper li').css({"opacity": "0"});
   $('.logo-wrapper h2').css({
     "transform": "scale(0)",
@@ -111,7 +113,7 @@ $(function() {
     });
   });
 
-  // ＊＊＊擬似要素NG＊＊＊animation5 ABOUT US  about-wrapper 点滅 flash
+  // ＊＊＊　200323＊＊＊animation5 ABOUT US  about-wrapper 点滅 flash
   $(window).scroll(function (){
     $('.about-wrapper h2, .about-wrapper p').each(function () {
       let position = $(this).offset().top;
@@ -130,12 +132,18 @@ $(function() {
 // animation6 WHY CHOOSE US　左右ブルブル　アニメーション用 buruburu
 
 $(window).scroll(function (){
-  $('.whychoose-wrapper h2, .whychoose-text').each(function () {
+  $('.whychoose-wrapper h2, .border-buruburu, .whychoose-text').each(function () {
     let position = $(this).offset().top;
     let scroll = $(window).scrollTop();
     let windowHeight = $(window).height();
     if (scroll > position - windowHeight + windowHeight/2) {
-      $(this).addClass('buruburu');
+      $('.whychoose-wrapper h2').addClass('buruburu');
+      setTimeout(function() {
+        $('.whychoose-wrapper h2').next().addClass('buruburu');
+      },500);
+      setTimeout(function() {
+        $('.whychoose-wrapper h2').next().next().addClass('buruburu');
+      },750);
       $('.whychoose-wrapper').css("overflow", "hidden");
     } else {
       $(this).removeClass('buruburu');
@@ -143,10 +151,7 @@ $(window).scroll(function (){
   });
 });
 
-
-  // ***これから*** animation7(animation3と同じ感じ) Get a quote now.　前後少し回転
-
-
+//***200323*** animation7 （animation3と同じ感じ）＜CSS keyframes使ってみる＞ Pure Creativity 縦回転
 
 
   // animation8　GET IN TOUCH　ズームイン
@@ -156,14 +161,15 @@ $(window).scroll(function (){
     "transition": "transform 2s"
     });
   $(window).on('scroll', function (e){
-    $('.getin-wrapper h2, .getin-wrapper p').each(function(){
+    $('.getin-wrapper h2,.border-zoom, .getin-wrapper p').each(function(){
       let position = $(this).offset().top;
       let scroll = $(window).scrollTop();
       let windowHeight = $(window).height();
-      if (scroll > position - windowHeight + windowHeight/5){
+      if (scroll > position - windowHeight + windowHeight/3){
         $(this).css({
           "opacity": "1",
           "transform": "scale(1)",
+          "transition": "transform 1s"
           });
       } else {
         $(this).css({
